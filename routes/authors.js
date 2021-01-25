e7480783e17b8101cc27f72fa7ea9d39df04ff18
oscarleonnogales <oscarleonnogales@gmail.com>
2021-01-25 13:51:26 -0800

    Created book index

--- routes/authors.js
+++ routes/authors.js
@@ -10,7 +10,6 @@ router.get('/', async (req, res) => {
 	}
 	try {
 		const authors = await Author.find(searchOptions);
-		console.log(searchOptions);
 		res.render('authors/index', { authors: authors, searchOptions: req.query });
 	} catch {
 		res.redirect('/');

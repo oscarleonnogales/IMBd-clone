9402f316ea161337b28cc6f45cfe2b0b0a0d6792
oscarleonnogales <oscarleonnogales@gmail.com>
2021-01-24 21:14:27 -0800

    Added search functionality to authors

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const Author = require('../models/author');
 
 // All authors
-router.get('/', (req, res) => {
-	res.render('authors/index');
+router.get('/', async (req, res) => {
+	let searchOptions = {};
+	if (req.query.name != null && req.query.name !== '') {
+		searchOptions.name = new RegExp(req.query.name, 'i');
+	}
+	try {
+		const authors = await Author.find(searchOptions);
+		console.log(searchOptions);
+		res.render('authors/index', { authors: authors, searchOptions: req.query });
+	} catch {
+		res.redirect('/');
+	}
 });
 
 // New Author
@@ -13,8 +23,21 @@ router.get('/new', (req, res) => {
 });
 
 // Create Author
-router.post('/', (req, res) => {
-	res.send('create');
+router.post('/', async (req, res) => {
+	const author = new Author({
+		name: req.body.name,
+	});
+	try {
+		const newAuthor = await author.save();
+		// res.redirect(`authors/${newAuthor.id}`)
+		res.redirect('authors');
+	} catch {
+		const locals = {
+			author: author,
+			errorMessage: 'Error creating a new author',
+		};
+		res.render('authors/new', locals);
+	}
 });
 
 module.exports = router;
